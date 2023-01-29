6fadc8fd9506cde2cddf230b1bd21e6bc8f80796
Mario Vellandi <mvellandi@hotmail.com>
2023-01-29 15:16:51 +0100

    ~ Update for GH pages and demo

--- AS-header-before-and-after.php
+++ AS-header-before-and-after.php
@@ -1,9 +1,9 @@
-<!-- NEW, IMPROVED HEADER -->
+<!-- START MODIFIED HEADER -->
 <!-- Cleaned, removed viewport plugin script, added standard meta viewport tag for responsive design  -->
 
 <!DOCTYPE html>
 <html class="no-js" <?php language_attributes(); ?>>
-
+  
 <head>
   <meta charset="<?php bloginfo('charset'); ?>">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -13,12 +13,15 @@
 
 <body <?php body_class(); ?> <?php wp_body_open(); ?> <?php do_action('fsn_base_header_components'); ?>
 
-<!-- OLDER, BROKEN HEADER -->
-<!-- appeared to be missing a mobile detection script, which is why the viewport meta tag was not being added to the header. -->
+<!-- END MODIFIED HEADER -->
+
+
+<!-- START ORIGINAL, BROKEN HEADER -->
+<!-- It appeared to be missing a mobile detection script (plugin), which is why the viewport meta tag was not being added to the header. -->
 
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta charset="<?php bloginfo( 'charset' ); ?>">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <?php
 $options = get_option( 'fsn_options' );
 $fsn_meta_viewport = !empty($options['fsn_meta_viewport']) ? $options['fsn_meta_viewport'] : '';
@@ -36,5 +39,7 @@ if (!empty($fsn_meta_viewport)) {
 <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?> data-view="mobile">
-    <?php wp_body_open(); ?>
+  <?php wp_body_open(); ?>
 	<?php do_action('fsn_base_header_components'); ?>
+
+  <!-- END ORIGINAL, BROKEN HEADER -->
--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 
 ## Summary
 
-Geoffrey Golden is a narrative writer for gaming and entertainment companies. Since 2019, he's been publishing [Adventure Snack](https://www.adventuresnack.com/), a “Choose Your Own Adventure” fiction magazine. While the story introduction pages had a fairly good design, the branching narrative pages’ layout and typography were too small, and were illegible on mobile. I redesigned those pages to better use available space and to read comfortably on multiple screen sizes.
+This is a story-page redesign for [Adventure Snack](http://www.adventuresnack.com/) an interactive fiction magazine.
 
 [See full project details here](http://www.vellandi.net/projects/adventure-snack)
+
+[Demo: Before](http://mvellandi.github.io/adventure-snack/index-before.html)
+[Demo: After](http://mvellandi.github.io/adventure-snack/index-after.html)
+
+Code changes before and after modifications, are available to browse in this repo.
